99f29ecd1e4e2bf3112cc4160330af0e7142f6db
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2020-03-21 15:29:11 +0800

    factory and seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+        'title' =>  $faker->sentence($faker->numberBetween(3,9)),
+        'body'  =>  $faker->paragraph($faker->numberBetween(5,10)),
+        'publish_at'    => now(),
+        'is_publish'    => 1,
+        'user_id' => $faker->numberBetween(1,51),
+        'category' => $faker->sentence(2),
     ];
 });
